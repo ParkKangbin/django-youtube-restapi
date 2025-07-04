376ac30118254864e385c98cb0ba29a57b3e24dc
ParkKangbin <6883997@naver.com>
2025-07-04 22:33:59 +0900

    django15일차

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(6je*08@!^@oefr0ip-zcj$ymcz=xxpchp-$jjdaq28uv=_7#-'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'test')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0))) # 0: False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
